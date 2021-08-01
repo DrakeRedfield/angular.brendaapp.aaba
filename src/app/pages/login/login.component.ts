379250b14da384b4a5349d81dbac9d69981b63e1
Angel Antonio Barco Alfaro <drake.redfield@hotmail.com>
2021-08-01 10:03:16 -0500

    Home Responsive, y navegacion desde login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,7 @@ export class LoginComponent implements OnInit {
   form: any;
 
   constructor(
+    private routes: Router,
   ) {
   }
 
@@ -21,6 +23,7 @@ export class LoginComponent implements OnInit {
     if( formLogin.invalid ){
       return;
     }
+    this.routes.navigate(['/home/'],{replaceUrl: true})
   }
 
 }
